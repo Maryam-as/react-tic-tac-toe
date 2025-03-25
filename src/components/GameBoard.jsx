32c53292a5e6edf1212c0d35124b7b5a21a55a61
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-26 00:24:25 +0330

    add event handler to square button to manage and update the gameBoard state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // define the structure for a 3x3 grid game board
 const initialGameBoard = [
   [null, null, null],
@@ -5,15 +6,31 @@ const initialGameBoard = [
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    // update the gameBoard state based on the previous state of that gameBoard
+    setGameBoard((prevGameBoard) => {
+      /*
+      the state that depends on objects or arrays - reference values - should be updated in an immutable way: create a copy of the old state, then change the copy
+      */
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     // render the game board grid dynamically based on initialGameBoard data
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
