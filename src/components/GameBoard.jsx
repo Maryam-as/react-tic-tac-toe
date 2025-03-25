053176558b28d046968307155dac1c3f72a16440
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-25 20:40:29 +0330

    define the structure for a 3x3 grid game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,9 @@
+// define the structure for a 3x3 grid game board
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard() {
+  return;
+}
