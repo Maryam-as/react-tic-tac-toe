17b278b814789e0f63ef8207539f569f614beefc
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-25 22:01:47 +0330

    render the game board grid dynamically based on initialGameBoard data

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,5 +5,20 @@ const initialGameBoard = [
   [null, null, null],
 ];
 export default function GameBoard() {
-  return;
+  return (
+    // render the game board grid dynamically based on initialGameBoard data
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
