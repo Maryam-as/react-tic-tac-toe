516904dd516d4b113509e0bdb99c0878e977aaa1
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-25 13:11:44 +0330

    add event handler on input and manage the state for player's name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
-
-export default function Player({ name, symbol }) {
-  // Store the information if the Player is currently being edited
+export default function Player({ initialName, symbol }) {
+  // initialize state for the player's name and provide a function to update it
+  const [playerName, setPlayerName] = useState(initialName);
+  // track whether the player is in edit mode (true if editing, false otherwise)
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
     // update the new state based on the previous state value
     setIsEditing((editing) => !editing);
   }
-  let playerName = <span className="player-name">{name}</span>;
+  // update playerName state with the value from the input field
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  // output this span when NOT in Edit mode
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // output this input when in Edit mode
   if (isEditing) {
     // automatically fill this input field with the current player's name
-    playerName = <input type="text" value={name} required />;
+    editablePlayerName = (
+      // two-way data binding for user input: updates playerName state on change
+      <input type="text" value={playerName} onChange={handleChange} required />
+    );
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       {/* display the button's caption dynamically, depending on its current state */}
