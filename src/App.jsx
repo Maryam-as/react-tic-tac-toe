92a288ab49db30cb08bb80512945eb66fd4b5c1e
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-26 14:05:27 +0330

    define a function to switch turns between players
    handleSelectSquare() function should be executed every time we want to
    switch turns after a sqaure is selected on the game board

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import Player from "./components/Player.jsx";
 function App() {
   // manage the activePlayer state in App component which has access to both Player & GameBoard components that need to work with this state
   const [activePlayer, setActivePlayer] = useState("x");
+  // Define a function to switch turns after a square is selected on the game board
+  function handleSelectSquare() {
+    // Update the state to toggle between 'X' and 'O' for the next player's turn
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X"
+    );
+  }
   return (
     <main>
       <div id="game-container">
