358ecb1f142e9536a615245c88c19aa183bda25a
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-26 17:38:09 +0330

    delete the gameBoard state in the GameBoard component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
 // define the structure for a 3x3 grid game board
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex, colIndex) {
-    // update the gameBoard state based on the previous state of that gameBoard
-    setGameBoard((prevGameBoard) => {
-      // the state that depends on objects or arrays - reference values - should be
-      // updated in an immutable way: create a deep copy of the old state, then change the copy
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol; // mark the selected square with the current player's symbol
-      return updatedBoard;
-    });
-    // call onSelectSquare function from inside of handleSelectSquare function because this function is triggered if a square was selected by clicking the button
-    onSelectSquare();
-  }
+export default function GameBoard({ onSelectSquare }) {
   return (
     // render the game board grid dynamically based on initialGameBoard data
     <ol id="game-board">
@@ -29,9 +13,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
-                  {playerSymbol}
-                </button>
+                <button onClick={onSelectSquare}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
