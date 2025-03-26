1dc06fbb076020c1a9a3cb7ddbe49d414b0dc90b
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-03-26 13:51:54 +0330

    manage the activePlayer state in the App component
    lifting the state up: manage and update the activePlayer state in the
    closest ancestor which has access to the both Player and GameBoard
    components that need to work with this activePlayer state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 
 function App() {
+  // manage the activePlayer state in App component which has access to both Player & GameBoard components that need to work with this state
+  const [activePlayer, setActivePlayer] = useState("x");
   return (
     <main>
       <div id="game-container">
